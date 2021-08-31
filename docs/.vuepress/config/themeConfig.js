4a128b845ed552775968807d7c42111edf3bfedb
yangxin6 <2362813136@qq.com>
2021-08-31 12:50:04 +0800

    init

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -79,7 +79,7 @@ module.exports = {
     // 页脚信息
     createYear: 2021, // 博客创建年份
     copyrightInfo:
-      'Coderhyp | <a href="https://github.com/yangxin6/blog" target="_blank">GITHUB</a>', // 博客版权信息，支持a标签
+      'yangxin | <a href="https://github.com/yangxin6/blog" target="_blank">GITHUB</a>', // 博客版权信息，支持a标签
   },
   htmlModules // 插入hmtl(广告)模块
 }
