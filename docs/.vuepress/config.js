8c8d07a4f02a7a1c01a2bcac60cd4826d1f41e52
yangxin6 <2362813136@qq.com>
2021-09-01 14:12:08 +0800

    vuepress-plugin-mathjax 数学公式

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,23 +11,18 @@ module.exports = {
   base: "/blog/", // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
-    extendMarkdown: (md) => {
-      md.set({
-        html: true,
-      });
-      md.use(require("markdown-it-katex"));
-    },
   },
-  head: [
-    ['link', {
-        rel: 'stylesheet',
-        href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css'
-    }],
-    ['link', {
-        rel: "stylesheet",
-        href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css"
-    }]
-  ],
-  plugins,
+  head,
+  plugins: [
+    [
+      'vuepress-plugin-mathjax',
+      {
+        target: 'svg',
+        macros: {
+          '*': '\\times',
+        },
+      },
+    ],
+  ],,
   themeConfig,
 };
