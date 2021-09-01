c09fd57f8e8d6b7964fd1b8210c189c4422add57
yangxin6 <2362813136@qq.com>
2021-09-01 14:00:36 +0800

    markdown-it-katex 数学公式插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,24 +1,33 @@
-const head = require('./config/head.js');
-const plugins = require('./config/plugins.js');
-const themeConfig = require('./config/themeConfig.js');
+const head = require("./config/head.js");
+const plugins = require("./config/plugins.js");
+const themeConfig = require("./config/themeConfig.js");
 
 module.exports = {
-  theme: 'vdoing', // 使用npm包主题
+  theme: "vdoing", // 使用npm包主题
   // theme: require.resolve('../../theme-vdoing'), // 使用本地主题
 
   title: "yangxin",
-  description: '',
-  base: '/blog/', // 格式：'/<仓库名>/'， 默认'/'
+  description: "",
+  base: "/blog/", // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
-    extendMarkdown: md => {
-      md.use(require('markdown-it-katex')) // 启动 markdown-it-katex 插件
-    }
+    extendMarkdown: (md) => {
+      md.set({
+        html: true,
+      });
+      md.use(require("markdown-it-katex"));
+    },
   },
   head: [
-    ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css' }],
-    ['link', { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css" }]
+    ['link', {
+        rel: 'stylesheet',
+        href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css'
+    }],
+    ['link', {
+        rel: "stylesheet",
+        href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css"
+    }]
   ],
   plugins,
   themeConfig,
-}
+};
