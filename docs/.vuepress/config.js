7fc457b616f99d1d4eb7ae7bef873c2b56c3e3bd
yangxin6 <2362813136@qq.com>
2021-09-01 13:47:55 +0800

    markdown-it-katex 数学公式插件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -11,12 +11,10 @@ module.exports = {
   base: '/blog/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
-    config: md => {
-      md.set({html: true})
-      md.use(require("markdown-it-katex"))
+    extendMarkdown: md => {
+      md.use(require('markdown-it-katex')) // 启动 markdown-it-katex 插件
     }
   },
-
   head: [
     ['link', { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.7.1/katex.min.css' }],
     ['link', { rel: "stylesheet", href: "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css" }]
